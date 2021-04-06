f218448678b8a60232164bc385e220dafd46098c
Evandro Cardoso Lippert Junior <evandro.lippert@gmail.com>
2021-04-06 13:47:24 -0300

    Added images test, image field at model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,5 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -6,6 +8,13 @@ from django.contrib.auth.models import (
 )
 
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate File Path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -77,6 +86,7 @@ class Recipe(models.Model):
     # O parâmetro inserido dentro do ManyToManyField é o nome da classe que será relacionada
     ingredients = models.ManyToManyField("Ingredient")
     tags = models.ManyToManyField("Tag")
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
